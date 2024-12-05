f9f714aaf0c548190fdfeec7448a9509ca6f836e
Darnel Castor <darnelcastor2@gmail.com>
2024-12-05 09:33:47 -0500

    added files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,40 @@
+"use client";
+
+import { useState } from "react";
+
+interface Message {
+  sender: string;
+  content: string;
+}
+
 export default function Home() {
-  return <div>hello world</div>;
+  const [messages, setMessages] = useState<Message[]>([
+    { sender: "John", content: "Hello!" },
+    { sender: "Jane", content: "Hi there!" },
+  ]);
+  const [input, setInput] = useState("");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setMessages([...messages, { sender: "Jhon", content: input }]);
+    setInput("");
+  };
+  return (
+    <div>
+      {messages.map((message, index) => (
+        <div key={index}>
+          <strong>{message.sender}</strong> : {message.content}
+        </div>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="message"
+          id="message"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 }
